acf53499b15ead3a9d30559ef9632ac0cbe932bc
Subhamoy Karmakar <subhamoykarmakar224@gmail.com>
2021-03-09 14:26:40 -0800

    Add new post scripts

--- code/src/index.js
+++ code/src/index.js
@@ -1,11 +1,38 @@
 const express = require("express");
+var bodyParser = require('body-parser');
+
 const app = express();
 const port = 8080;
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 
 // define a default route handler for the default home page
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
+
+app.get("/add/device", jsonParser, (req, res) => {
+  console.log("<<< ---- NEW DEVICE ---- >>>");
+  console.log("ID :: " + req.body.did);
+  console.log("Name :: " + req.body.name);
+  console.log("DateTime :: " + req.body.dateTime);
+  console.log("---------------------------------");
+});
+
+app.get("/add/data", jsonParser, (req, res) => {
+  console.log("<<< ---- NEW DEVICE ---- >>>");
+  console.log("ID :: " + req.body.sid);
+  console.log("Name :: " + req.body.name);
+  console.log("phone :: " + req.body.phone);
+  console.log("---------------------------------");
+});
+
+
 
 // start the Express server
 app.listen(port, () => {
